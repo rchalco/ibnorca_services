87198d4b845b3a806ecfe108f24bb73ed0add873
Ruben Dario Chalco Campos <nebur.dario@gmail.com>
2021-02-23 19:14:26 -0400

    consumiendo servicios

--- BackgroundAPIRest/Startup.cs
+++ BackgroundAPIRest/Startup.cs
@@ -31,13 +31,15 @@ namespace BackgroundAPIRest
                        .AllowAnyMethod()
                        .AllowAnyHeader();
             }));
+
+            services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
