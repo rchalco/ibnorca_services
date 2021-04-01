f1f0b989307b8b3a2340f7caae333cc1099083d7
ivilela <ivanvilela333@users.noreply.github.com>
2021-04-01 16:19:10 -0400

    Clases REPS

---
+++ Business.Main/Modules/ElaboracionAuditoria/Reportes/TCS/REPDatosDeLaOrganizacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Main.Modules.ElaboracionAuditoria.Reportes.TCS
+
+    /// <summary>
+    /// PLAN_REG-PRO-TCS-05-08.03 Datos de la organizacion (Ver 1.0)    /// </summary>
+public class REPDatosDeLaOrganizacion
+    {
+        public string FechaDeAuditoria { get; set; }
+        public string TipoDeAuditoria { get; set; }
+        public string NombreDeLaOrganizacion { get; set; }
+        public string Norma { get; set; }
+        public string Alcance { get; set; }
+        public string Sitios { get; set; }
+        public string NombreAuditorLider { get; set; }
+        public string NombreCargoRepresentanteOrg { get; set; }
+        public string CoordinadorAuditoria { get; set; }
+        public string FechaActual { get; set; }
+    }
+}
---
+++ Business.Main/Modules/ElaboracionAuditoria/Reportes/TCS/REPEvaluacionYRecomendacionesParaProceso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Main.Modules.ElaboracionAuditoria.Reportes.TCS
+{
+    /// <summary>
+    /// PLAN_REG-PRO-TCS-06-01_00 EvaluacionYRecomendacionParaUnProcesoDeCertificacion(Ver 1.0)
+    /// </summary>
+    public class REPEvaluacionYRecomendacionesParaProceso
+    {
+        public string Orgaizacion { get; set; }
+        public string CodigoDeServicio { get; set; }
+        public string FechaDeAuditoria { get; set; }
+        public string TipoDeAuditoria { get; set; }
+        public string NormasAuditadas { get; set; }
+        public string EquipoAuditoNombreCargo { get; set; }
+        public string ExpertoCertificacion { get; set; }
+        public string FechaAsignacionProceso { get; set; }
+        public string RedaccionSugerida { get; set; }
+    
+    }
+}
