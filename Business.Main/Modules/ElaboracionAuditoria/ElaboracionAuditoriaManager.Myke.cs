54e9df1c7d4dd61f9491a6e3759dc6951b5f21b4
mikyches <68037447+mikyches@users.noreply.github.com>
2021-04-01 17:16:41 -0400

    CAmbios

--- Business.Main/Modules/ElaboracionAuditoria/ElaboracionAuditoriaManager.Myke.cs
+++ Business.Main/Modules/ElaboracionAuditoria/ElaboracionAuditoriaManager.Myke.cs
@@ -4,6 +4,7 @@ using Business.Main.DataMapping.DTOs;
 using Business.Main.Modules.AperturaAuditoria.Domain.DTOWSIbnorca.BuscarxIdClienteEmpresaDTO;
 using Business.Main.Modules.AperturaAuditoria.Domain.DTOWSIbnorca.ListarAuditoresxCargoCalificadoDTO;
 using Business.Main.Modules.AperturaAuditoria.Domain.DTOWSIbnorca.ListarContactosEmpresaDTO;
+using Business.Main.Modules.ElaboracionAuditoria.Reportes.TCS;
 using Domain.Main.Wraper;
 using Newtonsoft.Json;
 using PlumbingProps.Document;
@@ -25,7 +26,7 @@ namespace Business.Main.Modules.ElaboracionAuditoria
         /// <param name="IdCiclo"></param>
         /// <param name="pathPlantilla"></param>
         /// <returns></returns>
-        public Response GenerarNotaSuspension(int IdCiclo, string pathPlantilla)
+        public Response GenerarNotaSuspension(int IdCiclo, string pathPlantilla, string nombre, string cargo, DateTime fechaLiteral)
         {
             Response response = new Response { Message = "", State = ResponseType.Success };
             try
@@ -62,87 +63,48 @@ namespace Business.Main.Modules.ElaboracionAuditoria
                 }
                 ContactoEmpresa contactoEmpresa = resulServices.lstContactos?.Count > 0 ? resulServices.lstContactos[0] : null;
 
-                ///llenamos el reporte con la informacion de este ciclo
-                RepDocDesignacion praDocDesignacion = new RepDocDesignacion
+                string normas = "";
+
+                praciclocronograma.Praciclonormassistemas.ToList().ForEach(x =>
                 {
-                    FechadeAuditoria = praciclocronograma.Praciclocronogramas.First().FechaInicioDeEjecucionDeAuditoria?.ToString("dd/MM/yyyy"),
-                    TipodeAuditoria = praciclocronograma.Referencia,
-                    ModalidaddeAuditoria = $"Días insitu: {praciclocronograma.Praciclocronogramas.First().DiasInsitu}, días remoto: {praciclocronograma.Praciclocronogramas.First().DiasRemoto}",
-                    FechaInicioAuditoria = praciclocronograma.Praciclocronogramas.First().FechaInicioDeEjecucionDeAuditoria?.ToString("dd/MM/yyyy"),
-                    FechaFinAuditoria = praciclocronograma.Praciclocronogramas.First().FechaDeFinDeEjecucionAuditoria?.ToString("dd/MM/yyyy"),
-                    CantidadDiasAuditor = praciclocronograma.Praciclocronogramas.First().DiasInsitu + praciclocronograma.Praciclocronogramas.First().DiasRemoto,
-                    OrganismoCertificador = praprogramasdeauditorium.OrganismoCertificador,
-                    CodigoDeServicioIbnorca = praprogramasdeauditorium.CodigoServicioWs,
-                    Organizacion = cliente.NombreRazon,
-                    AltaDireccion = contactoEmpresa?.NombreDrEncargado,
-                    CargoAltaDireccion = string.Empty,
-                    PersonaDeContacto = contactoEmpresa?.NombreContacto,
-                    CargoPersonaDeContacto = contactoEmpresa?.CargoContacto,
-                    TelefonoDeContacto = contactoEmpresa?.FonoContacto,
-                    CorreoElectronico = contactoEmpresa?.CorreoContacto,
-                    CodigoAIF = praprogramasdeauditorium.CodigoIafws,
-                    AlcanceDeCertificacion = string.Empty,
-                    HorarioHabitualDeTrabajo = praciclocronograma.Praciclocronogramas.First().HorarioTrabajo,
-                    FechaProximaAuditoria = praciclocronograma.Praciclocronogramas.First().MesProgramado?.AddYears(1).ToString("dd/MM/yyyy"),
-                    ListRepDesginacionParticipante = praciclocronograma.Pracicloparticipantes.Select(x =>
-                    {
-                        RepDesginacionParticipante repDesginacionParticipante = new RepDesginacionParticipante();
-                        repDesginacionParticipante.Cargo = string.Empty;
-                        if (!string.IsNullOrEmpty(x.CargoDetalleWs))
-                        {
-                            ListaCalificado cargo = JsonConvert.DeserializeObject<ListaCalificado>(x.CargoDetalleWs);
-                            repDesginacionParticipante.Cargo = cargo.CargoPuesto;
-                        }
+                    normas += x.Norma;
+                });
 
-                        repDesginacionParticipante.Participante = string.Empty;
-                        if (!string.IsNullOrEmpty(x.ParticipanteDetalleWs))
-                        {
-                            ListaCalificado participante = JsonConvert.DeserializeObject<ListaCalificado>(x.ParticipanteDetalleWs);
-                            repDesginacionParticipante.Participante = participante.NombreCompleto;
-                        }
+                string alcance = "";
+                praciclocronograma.Praciclonormassistemas.ToList().ForEach(x => 
+                {
+                    alcance += x.Alcance + WordHelper.GetCodeKey(WordHelper.keys.enter);
+                });
 
-                        return repDesginacionParticipante;
-                    }).ToList()
+                string sitios = "";
+                praciclocronograma.Pradireccionespasistemas.ToList().ForEach(x =>
+                {
+                    sitios += x.Direccion + WordHelper.GetCodeKey(WordHelper.keys.enter);
+                });
+
+                ///llenamos el reporte con la informacion de este ciclo
+                RepNotaSuspensionCertifica praSuspension = new RepNotaSuspensionCertifica
+                {
+                    FechaCabecera = "-" + DateTime.Now.ToString("dd/MM/yyyy"),
+                    CorrelativoCabecera = "--",
+                    NombreNota = nombre,
+                    Cargo = cargo,
+                    ReferenciaNota = normas,
+                    NroCertificado = "XXXX", // TODO:Pendiente 
+                    NombreEmpresaTexto = cliente.NombreRazon,
+                    DescripcionOtrogado = alcance,
+                    Sitios = sitios,
+                    FechaLiteral1 = "XXXXX", // TODO:Pendiente 
+                    Seguimiento = praciclocronograma.Referencia,
+                    FechaLiteral2 = fechaLiteral.ToString("dd/MM/yyyy"),
+                    DirectorEjecutivo = "XXXXXX" /// TODO:Pendiente
+                    
                 };
                 string filePlantilla = Global.PATH_PLANTILLA_DESIGNACION + pathPlantilla;
                 WordHelper generadorWord = new WordHelper(filePlantilla);
-                praDocDesignacion.SitiosAAuditar = string.Empty;
-                praciclocronograma.Pradireccionespasistemas?.ToList().ForEach(direccion =>
-                {
-                    praDocDesignacion.SitiosAAuditar += direccion.Direccion + WordHelper.GetCodeKey(WordHelper.keys.enter);
-                });
-
-                praciclocronograma.Praciclonormassistemas?.ToList().ForEach(alcance =>
-                {
-                    praDocDesignacion.AlcanceDeCertificacion += alcance.Norma + " - " + alcance.Alcance + WordHelper.GetCodeKey(WordHelper.keys.enter);
-                });
-
+               
                 //generamos el documento en word
-                Dictionary<string, CellTitles[]> pTitles = new Dictionary<string, CellTitles[]>();
-                CellTitles[] cellTitlesTitulo = new CellTitles[2];
-                cellTitlesTitulo[0] = new CellTitles { Title = "Calificación", Visible = true, Width = "50" };
-                cellTitlesTitulo[1] = new CellTitles { Title = "Auditor", Visible = true, Width = "50" };
-                pTitles.Add("ListRepDesginacionParticipante", cellTitlesTitulo);
-
-                string fileNameGenerado = generadorWord.GenerarDocumento(praDocDesignacion, pTitles, $"{Global.PATH_PLANTILLA_DESIGNACION}\\Salidas");
-
-                ///Convertimos en PDF
-                //using (var process = new Process())
-                //{
-                //    process.StartInfo.FileName = @"E:\ConvertPDF\ConvertExecute.exe"; // relative path. absolute path works too.
-                //    process.StartInfo.Arguments = $"{fileNameGenerado}";
-                //    process.StartInfo.CreateNoWindow = true;
-                //    process.StartInfo.UseShellExecute = false;
-                //    process.StartInfo.RedirectStandardOutput = true;
-                //    process.StartInfo.RedirectStandardError = true;
-                //    process.OutputDataReceived += (sender, data) => Console.WriteLine(data.Data);
-                //    process.ErrorDataReceived += (sender, data) => Console.WriteLine(data.Data);
-                //    process.Start();
-                //    process.BeginOutputReadLine();
-                //    process.BeginErrorReadLine();
-                //    process.WaitForExit();     // (optional) wait up to 10 seconds                    
-                //}
-                //response.Message = fileNameGenerado.Replace(".doc", ".pdf");
+                string fileNameGenerado = generadorWord.GenerarDocumento(praSuspension, null, $"{Global.PATH_PLANTILLA_DESIGNACION}\\Salidas");
                 response.Message = fileNameGenerado;
             }
             catch (Exception ex)
